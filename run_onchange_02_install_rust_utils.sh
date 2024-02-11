56fb1758301793e6119be697b2f110b9223625bd
lorefnon <lorefnon@tutanota.com>
2024-02-11 15:16:27 +0530

    Add ansible

--- run_onchange_02_install_rust_utils.sh
+++ run_onchange_02_install_rust_utils.sh
@@ -2,4 +2,4 @@
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-cargo ripgrep csvlens hbs-cli difftastic starship bat exa alacritty zoxide broot du-dust
+cargo install ripgrep csvlens hbs-cli difftastic starship bat exa alacritty zoxide broot du-dust
---
+++ run_onchange_10_setup_ansible.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+sudo apt-add-repository ppa:ansible/ansible
+
+sudo apt update
+
+sudo apt install ansible
